772aeac73ae11ff4766ab2c95b5221a9035ba8e1
Stucky <stoyan.v.stoyanov86@gmail.com>
2023-05-02 10:41:22 +0300

    adding confirmation for delete

--- OnBoardingProject.UI/Pages/AdminTools.razor.cs
+++ OnBoardingProject.UI/Pages/AdminTools.razor.cs
@@ -8,6 +8,8 @@ namespace OnBoardingProject.UI.Pages
     {
         public IEnumerable<ProductModel> Products { get; set; }
 
+        private ProductModel product;
+        private bool showConfirmDialog = false;
 
         [Inject]
         ProductsService ProductsService { get; set; }
@@ -17,9 +19,17 @@ namespace OnBoardingProject.UI.Pages
             Products = await ProductsService.GetProductsAsync();
         }     
         
-        private async Task OnDelete(Guid id)
+        private async Task OnDelete(ProductModel product)
         {
-            await ProductsService.DeleteProductAsync(id);//todo: add confirm modal
+            this.product = product;
+            showConfirmDialog = true;
+        }
+
+        private async Task Delete()
+        {            
+            await ProductsService.DeleteProductAsync(product.Id);
+            showConfirmDialog = false;
+            Products = await ProductsService.GetProductsAsync();
             StateHasChanged();
         }
     }
